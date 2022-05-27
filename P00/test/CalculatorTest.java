333a61be26075d0a6e5d04f3f4e92784949d2c3c
21015619 <21015619@YuXuan>
2022-05-27 14:33:51 +0800

    First Push to P00 repository

---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,54 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+public class CalculatorTest {
+	int a, b;
+	Calculator cal;
+	
+	@Before
+	public void setUp() throws Exception {
+		a = 4321;
+        b = 1234;
+		cal = new Calculator();
+	}
+	
+	@Test
+	public void testadd() {	
+		int actual = cal.add(a, b); 
+		int expected = 5555;
+		assertEquals(expected,actual);
+		}
+	@Test
+	public void testSub() {
+		int actual = cal.sub(a, b);	 
+		int expected = 3087;
+		assertEquals (expected, actual);
+		}
+
+	@Test
+	public void testMul() {
+		int actual = cal.mul(a, b);
+		
+		int expected = 5332114;
+		assertEquals (expected, actual);
+		}
+
+	@Test
+	public void testDivide() {
+		int actual = cal.div(a, b);	 
+		int expected = 3;
+		assertEquals (expected, actual);
+		}
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	
+	//	fail("Not yet implemented");
+	}
+
+
